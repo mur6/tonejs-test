4cb114dddb2bc1516aa36b3af28a23094c5e44fc
MURAKI Taichi <muraki.taichi@gmail.com>
2021-09-25 17:20:56 +0900

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,38 @@ import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import * as Tone from "tone";
 
 function App() {
   const [value, setValue] = useState(0);
+  const synth = new Tone.Synth().toDestination();
 
+  function playNote(note) {
+    synth.triggerAttackRelease(`${note}4`, "8n");
+  }
   return (
     <>
+    <button className="note" onClick={() => playNote("C")}>
+          C
+        </button>
+        <button className="note" onClick={() => playNote("D")}>
+          D
+        </button>
+        <button className="note" onClick={() => playNote("E")}>
+          E
+        </button>
+        <button className="note" onClick={() => playNote("G")}>
+          G
+        </button>
+        <button className="note" onClick={() => playNote("A")}>
+          A
+        </button>
       <Box>
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
+        <Button variant="outlined" onClick={() => playNote("C")}>C</Button>
+        <Button variant="outlined" onClick={() => playNote("D")}>D</Button>
+        <Button variant="outlined" onClick={() => playNote("E")}>E</Button>
+        <Button variant="outlined" onClick={() => playNote("G")}>G</Button>
+        <Button variant="outlined" onClick={() => playNote("A")}>A</Button>
       </Box>
       <BottomNavigation
         showLabels
